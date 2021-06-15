8ce594b6305b4b9ebf8dd95cc301ee0de1d419f9
CJ Bradshaw <coltonjbradshaw@gmail.com>
2021-06-14 22:26:11 -0600

    Started Visual Studio Conosle App
    Started basic Bank account in Console App.
    -Learned propful for easy Visual Studio creation of properties.

---
+++ CSharpPractice/Classes/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.Classes
+{
+    class BankAccount
+    {
+        public BankAccount()
+        {
+            Balance = 100;
+        }
+        private double Balance;
+        public double balance {
+            get
+            {
+                if (Balance < 100000)
+                    return 1000000;
+                else
+                    return Balance;
+            }
+            private set
+            {
+                if (value > 0)
+                    Balance = value;
+                else
+                    balance = 0;
+            }
+        }
+        public double Deposit(double depositAmount)
+        {
+            return balance += depositAmount;
+        }
+        public double Withdraw(double amount)
+        {
+            if(amount > Balance)
+            {
+                Console.WriteLine("Transaction denied, insuficient funds.");
+                return Balance;
+            }
+            else
+            {
+                return balance -= amount;
+            }
+        }
+        public string CheckBalance()
+        {
+            return "You have " + Balance.ToString() + " in your account.";
+        }
+    }
+}
---
+++ CSharpPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CSharpPractice.Classes;
+
+namespace CSharpPractice
+{
+    class Program
+    {
+        static BankAccount myAccount = new BankAccount();
+        static void Main(string[] args)
+        {
+            string myAccountBalance = myAccount.CheckBalance();
+            Console.WriteLine(myAccountBalance);
+            string message = Console.ReadLine();
+            Console.WriteLine("Echo:" + message);
+            Console.ReadLine();
+        }
+    }
+}
